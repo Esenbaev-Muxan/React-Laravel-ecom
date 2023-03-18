68a68e48719aab4da73be0a5fc6ca0dfeb318086
Esenbaev-Muxan <global-esenbaevmuxan@gmail.com>
2023-03-18 15:37:23 +0500

    register page

--- laravelcom/app/Http/Controllers/API/AuthController.php
+++ laravelcom/app/Http/Controllers/API/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -18,11 +19,23 @@ class AuthController extends Controller
 
         if($validator->fails())
         {
-
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ]);
         }
         else 
         {
-            
+            $user = User::create([
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'password'=>$request->password,
+            ]);
+
+            $token = $user->createToken($user->email.'_Token')->plainTextToken;
+
+            return response()->json([
+                'status'=>$validator->messages(),
+            ]);
         }
     }
 }
